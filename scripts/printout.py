effc257fcda46317aeb65ef1c4d50e1793e4920d
Ramiro Polla <ramiro.polla@gmail.com>
2014-11-09 18:09:32 +0100

    printout.py

--- scripts/printout.py
+++ scripts/printout.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import numpy as np
-import scipy.misc.pilutil as smp
-import scipy.misc
+from scipy.misc import toimage
+from scipy.misc import imsave
 import sys
 import re
 
@@ -19,19 +19,20 @@ max_x = -10000
 min_x =  10000
 
 for line in fp:
-	split = line.split(' ')
-	y = int(split[1])
-	if y == 200:
-		continue
-	if  max_y < y:
-		max_y = y
-	if  min_y > y:
-		min_y = y
-	x = int(split[2])
-	if  max_x < x:
-		max_x = x
-	if  min_x > x:
-		min_x = x
+	if "FIRE0" in line:
+		split = line.split(' ')
+		y = int(split[1])
+		if y == 200:
+			continue
+		if  max_y < y:
+			max_y = y
+		if  min_y > y:
+			min_y = y
+		x = int(split[2])
+		if  max_x < x:
+			max_x = x
+		if  min_x > x:
+			min_x = x
 
 print("X[%d:%d]" % (min_x, max_x))
 print("Y[%d:%d]" % (min_y, max_y))
@@ -48,32 +49,33 @@ data = np.zeros((height,width))
 fp.seek(0)
 
 for line in fp:
-	split = line.split(' ')
-	y = int(split[1])
-	y = y - min_y + 72
-	y = y / 8
-	x = int(split[2])
-	v = int(split[3], 16)
-	if v & 0x100:
-		data[height-1-(y+0)][x] = 255
-	if v & 0x001:
-		data[height-1-(y+1)][x] = 255
-	if v & 0x002:
-		data[height-1-(y+2)][x] = 255
-	if v & 0x004:
-		data[height-1-(y+3)][x] = 255
-	if v & 0x008:
-		data[height-1-(y+4)][x] = 255
-	if v & 0x010:
-		data[height-1-(y+5)][x] = 255
-	if v & 0x020:
-		data[height-1-(y+6)][x] = 255
-	if v & 0x040:
-		data[height-1-(y+7)][x] = 255
-	if v & 0x080:
-		data[height-1-(y+8)][x] = 255
+	if "FIRE0" in line:
+		split = line.split(' ')
+		y = int(split[1])
+		y = y - min_y + 72
+		y = y / 8
+		x = int(split[2])
+		v = int(split[3], 16)
+		if v & 0x100:
+			data[height-1-(y+0)][x] = 255
+		if v & 0x001:
+			data[height-1-(y+1)][x] = 255
+		if v & 0x002:
+			data[height-1-(y+2)][x] = 255
+		if v & 0x004:
+			data[height-1-(y+3)][x] = 255
+		if v & 0x008:
+			data[height-1-(y+4)][x] = 255
+		if v & 0x010:
+			data[height-1-(y+5)][x] = 255
+		if v & 0x020:
+			data[height-1-(y+6)][x] = 255
+		if v & 0x040:
+			data[height-1-(y+7)][x] = 255
+		if v & 0x080:
+			data[height-1-(y+8)][x] = 255
 
-img = smp.toimage(data)
-scipy.misc.imsave(fname_out, img)
+img = toimage(data)
+imsave(fname_out, img)
 
 fp.close()
